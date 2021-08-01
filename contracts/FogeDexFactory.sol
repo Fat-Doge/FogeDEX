7df5027c6dd2124393b86e4f469b51428a3ef799
Fat-Doge <development@fatdoge.finance>
2021-08-01 07:32:47 -0400

    Added router and additional files

--- contracts/FogeDexFactory.sol
+++ contracts/FogeDexFactory.sol
@@ -25,7 +25,7 @@ contract FogeDexFactory is IFogeDexFactory {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'FogeDex: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'FogeDex: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        bytes memory bytecode = type(FogeDexPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
